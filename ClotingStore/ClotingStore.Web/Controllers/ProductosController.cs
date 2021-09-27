9a40de665c4ef6e42bfcd673bca1e7efad2a9ba8
ARLETH CAROLINA HENRRIQUEZ PERDOMO <arleth.henrriquez@unah.hn>
2021-09-26 21:44:41 -0600

    Agragando Arquitectura Basica

--- ClotingStore/ClotingStore.Web/Controllers/ProductosController.cs
+++ ClotingStore/ClotingStore.Web/Controllers/ProductosController.cs
@@ -1,4 +1,5 @@
-﻿using ClotingStore.Web.Models;
+﻿
+using ClotingStore.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,26 +13,8 @@ namespace ClotingStore.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Camisa Top";
-
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 1;
-            producto2.Descripcion = "Blazer";
-
-
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 1;
-            producto3.Descripcion = "Pantalon Acampanado ";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
 
 
             return View(listadeProductos);
